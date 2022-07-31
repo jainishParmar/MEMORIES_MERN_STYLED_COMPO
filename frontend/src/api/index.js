bc59f275c33e157cfc0d11931d42b2e4c6328ca0
jainu-50 <parmarjainish20@gmail.com>
2022-08-01 00:44:20 +0530

    change api url

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://fant-memories.herokuapp.com/",
 });
 
 API.interceptors.request.use((req) => {
